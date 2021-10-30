8f55a1a91c9493c7fcf039c536af253f06ca9d34
Adnan20201 <afifuladnan2020@gmail.com>
2021-10-30 19:18:46 +0600

    Add body

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 $(function ($){
     'use strict';
 
+
+
    // toggle menu
    var logo = $('.logo');
    var menu = $('.myNavContent ul.nav');
@@ -14,4 +16,80 @@ $(function ($){
    //Pogo Slider
 
    var mySlider = $('.pogoSlider').pogoSlider({}).data('');
+
+   // Service-slider
+   $('.service-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+   
+   });
+
+   // Portfolio-slider
+   $('.portfolio-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+   
+   });
+
+   // Blog Slider
+   $('.blog-slider').slick({
+    prevArrow:'<a href=""><i class="slider-arrow slider-arrow-left fas fa-chevron-left"></i></a>',
+    nextArrow:'<a href=""><i class="slider-arrow slider-arrow-right fas fa-chevron-right"></i></a>',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    centerMode: true,
+    centerPadding: '0px',
+   
+   });
+
+//client-feedback slider
+
+   $('.client-feedback-slider').slick({
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    adaptiveHeight: 'client-img-slider',
+    fade: true,
+   });
+
+//client-img Slider
+   $('.client-img-slider ').slick({
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 10000,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '0px',
+    adaptiveHeight: 'client-feedback-slider',
+    
+   });
+
+
+//Brand  slider
+   $('.brand-slider').slick({
+    prevArrow:'<a href=""><i class="slider-arrow slider-arrow-left fas fa-chevron-left"></i></a>',
+    nextArrow:'<a href=""><i class="slider-arrow slider-arrow-right fas fa-chevron-right"></i></a>',
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 10000,
+    centerMode: true,
+    centerPadding: '0px',
+    
+   });
+
+   
+
+   
+   
 })
